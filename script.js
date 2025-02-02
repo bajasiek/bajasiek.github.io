c440aa834d7a452973518cffb5aa664dcbbada03
bajasiek <bajasiek@gmail.com>
2025-02-02 22:53:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayYes() {
+    document.getElementById("question").innerText = "Dziękuję, że jesteś <3 🥰";
+    document.getElementById("response").innerText = "💖💖💖";
+}
+
+function sayNo() {
+    document.getElementById("question").innerText = "Przemyśl to dupo :( ! 😢";
+    document.getElementById("response").innerText = "💔💔💔";
+}
